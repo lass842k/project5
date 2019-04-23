78008077a5f5a13f48a097aa8f5de51b9f8f5a59
lass842k <lassebaikjepsen@gmail.com>
2019-04-24 01:58:33 +0200

    Added JS & changes

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function validateForm() {
 
 let documentHeight = window.document.body.scrollHeight;
 let windowHeight = window.innerHeight;
-let maxHeight = documentHeight - windowHeight;
+let maxHeight = windowHeight - documentHeight;
 
 document.getElementById("progressBar").max = maxHeight;
 document.getElementById("progressBar").value = window.scrollY;
@@ -17,3 +17,5 @@ document.addEventListener("scroll", function() {
     document.getElementById("progressBar").value = window.scrollY;
     
 })
+
+/* Resten af javascript koderne er i <script> i HTML-filen da javascripten ikke loader mere herfra, pga. en fejl i progressBar javascripten. Har prøvet alt, og kan ikke få det maxHeighten til at virke */
